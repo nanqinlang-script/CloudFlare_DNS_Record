527f8744c077b7b61ee9d904b19e28aa1de64af7
nanqinlang <contacts@sometimesnaive.org>
2018-04-05 00:10:57 +0800

    script

--- CloudFlare_DDNS_Setter.sh
+++ CloudFlare_DDNS_Setter.sh
@@ -4,7 +4,7 @@ Info="${Green_font}[Info]${Font_suffix}"
 Error="${Red_font}[Error]${Font_suffix}"
 echo -e "${Green_font}
 #=======================================
-# Project: CloudFlare_DDNS_Keeper
+# Project: CloudFlare_DDNS_Setter
 # Version: 1.0
 # Author: nanqinlang
 # Blog:   https://sometimesnaive.org
@@ -17,6 +17,10 @@ check_root(){
 }
 
 check_system(){
+	[[ -z "`cat /etc/issue | grep -E -i "debian"`" && -z "`cat /etc/issue | grep -E -i "ubuntu"`" && -z "`cat /etc/redhat-release | grep -E -i "CentOS"`" ]] && echo -e "${Error} only support Debian or Ubuntu or CentOS !" && exit 1
+}
+
+check_deps(){
 	if  [[ ! -z "`cat /etc/issue | grep -E -i "debian"`" ]]; then
 		apt-get install -y openssl libssl-dev ca-certificates curl
 	elif
@@ -42,27 +46,34 @@ define(){
 	zone_id=`cat config.conf | grep "zone_id" | awk -F "=" '{print $NF}'`
 	api_key=`cat config.conf | grep "api_key" | awk -F "=" '{print $NF}'`
 
+	record_id=`cat config.conf | grep "record_id" | awk -F "=" '{print $NF}'`
 	domain=`cat config.conf | grep "domain" | awk -F "=" '{print $NF}'`
+	ttl=`cat config.conf | grep "ttl" | awk -F "=" '{print $NF}'`
 
 	dynamic_ip=`curl ip.sb`
-
-	ttl=`cat config.conf | grep "ttl" | awk -F "=" '{print $NF}'`
-	record_id=`cat config.conf | grep "record_id" | awk -F "=" '{print $NF}'`
 }
 
 choose_service(){
-	echo -e "${Info} select required service:\n1.get domain records details\n2.modify exists domain record\n3.create new domain record"
-	read -p "(input 1 or 2 or 3 to select):" service
+	if [[ -z "$1" ]]; then
+		echo -e "${Info} if you want a automatic ddns, firstly you should get record_id"
+		echo -e "${Info} alternatively you can use this script to create a A record and get its id"
+		echo -e "${Info} now select required service:\n1.get domain record_id\n2.create a new domain A record"
+		read -p "(input 1 or 2 to select):" service
+		while [[ ! "${service}" =~ ^[1-2]$ ]]
+		do
+			echo -e "${Error} invalid input !"
+			read -p "(input 1 or 2 to select):" service
+		done
+		[[ "${service}" = "1" ]] && get_record_id
+		[[ "${service}" = "2" ]] && create_record
 
-	while [[ ! "${service}" =~ ^[1-3]$ ]]
-	do
-		echo -e "${Error} invalid input !"
-		read -p "(input 1 or 2 or 3 to select):" service
-	done
+	elif [[ "$1" == "--ddns" ]]; then
+		echo -e "${Info} now will start automatically ddns record updating service"
+		update_record
 
-	[[ "${service}" = "1" ]] && get_record_id
-	[[ "${service}" = "2" ]] && update_record
-	[[ "${service}" = "3" ]] && create_record
+	else
+		echo -e "${Error} invalid input !" && exit 1
+	fi
 }
 
 get_record_id(){
@@ -92,6 +103,7 @@ curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records"
 
 check_root
 check_system
+check_deps
 directory
 define
-choose_service
+choose_service $1
