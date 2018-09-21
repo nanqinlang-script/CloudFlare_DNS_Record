77277121b885de83ca451756fb0907096167ffa6
dovela <33590269+dovela@users.noreply.github.com>
2018-09-21 20:43:32 +0800

    还是这个网站好点，响应快

--- CloudFlare_DDNS_Setter.sh
+++ CloudFlare_DDNS_Setter.sh
@@ -56,7 +56,7 @@ define(){
 	domain=`cat ${ddns_conf} | grep "domain" | awk -F "=" '{print $NF}'`
 	ttl=`cat ${ddns_conf} | grep "ttl" | awk -F "=" '{print $NF}'`
 
-	dynamic_ip=`curl ifconfig.me`
+	dynamic_ip=`curl ipv4.ip.sb`
 }
 
 choose_service(){
